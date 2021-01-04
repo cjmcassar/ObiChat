8f0b1ef8b4dbcc750e9101fa97624e87ffea4803
Christopher-John Cassar <christopherjcassar@gmail.com>
2021-01-04 11:50:48 +0000

    - added login feature - added logout feature - added authstatechange to keep track of user's authentication status

--- public/js/fbauth.js
+++ public/js/fbauth.js
@@ -1,17 +1,71 @@
-// register
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-e.preventDefault();
+  const signupForm = document.querySelector('#signup-form');
+  const logout = document.querySelector('#logout');
+  const loginForm = document.querySelector('#login-form');
 
-  // get user info
+  // listen for auth status changes
+  auth.onAuthStateChanged(user =>
+  {
+    if (user)
+    {
+      console.log("user logged in:", user);
+    }
+    else
+    {
+      console.log("user logged out");
+    }
 
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
-
-  // register the user
-
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred);
-    signupForm.reset();
   });
-});
+
+
+  if (document.querySelector('#signup-form') !== null)
+  {
+    // register
+    signupForm.addEventListener('submit', (e) =>
+    {
+      e.preventDefault();
+
+      // get user info
+
+      const email = signupForm['signup-email'].value;
+      const password = signupForm['signup-password'].value;
+
+      // register the user
+
+      auth.createUserWithEmailAndPassword(email, password).then(cred =>
+      {
+        // console.log(cred.user);
+        signupForm.reset();
+      });
+    });
+  }
+  else if (document.querySelector('#login-form') !== null)
+  {
+    //login
+    loginForm.addEventListener('submit', (e) =>
+    {
+      e.preventDefault();
+
+      //get user info
+
+      const loginemail = loginForm['login-email'].value;
+      const loginpassword = loginForm['login-password'].value;
+
+      // register the user
+
+      auth.signInWithEmailAndPassword(loginemail, loginpassword).then(cred =>
+      {
+        // console.log(cred.user);
+        // loginForm.reset();
+      });
+    });
+  }
+  //logout
+
+  logout.addEventListener('click', (e) =>
+  {
+    e.preventDefault();
+    auth.signOut(); //.then(() =>
+    // {
+    //   console.log("User signed out");
+    // });
+  });
