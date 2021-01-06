d8f32d9c73be0faa63e4406623a483c9a4611819
Christopher-John Cassar <christopherjcassar@gmail.com>
2021-01-06 17:56:15 +0000

    - rebased entire website. - got rid of ejs templates - cleaned up code location

--- start.js
+++ start.js
@@ -1,18 +1,36 @@
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const bodyParser = require("body-parser");
 const path = require('path');
 const express = require('express');
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:  "https://obivision-7645d-default-rtdb.firebaseio.com",
+});
+
+const csrfMiddleware = csrf({ cookie: true });
 
 const PORT = process.env.PORT || 3000;
+
+// --------------- autodesk api configuration
 const config = require('./config');
 if (config.credentials.client_id == null || config.credentials.client_secret == null) {
     console.error('Missing FORGE_CLIENT_ID or FORGE_CLIENT_SECRET env. variables.');
     return;
 }
+// --------------- end of autodesk api configuration ------ 
 
 let app = express();
 app.engine("html", require("ejs").renderFile);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '50mb' }));
+
+// --------------- autodesk api configuration ------ 
 app.use('/api/forge/oauth', require('./routes/oauth'));
 app.use('/api/forge/oss', require('./routes/oss'));
 app.use('/api/forge/modelderivative', require('./routes/modelderivative'));
@@ -20,6 +38,18 @@ app.use((err, req, res, next) => {
     console.error(err);
     res.status(err.statusCode).json(err);
 });
+// --------------- end of autodesk api configuration ------ 
+
+// --------------- cookie configuration ------ 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
+// --------------- end of cookie configuration ------ 
 
 app.get("/login", function (req, res) {
   res.render("login.html");
@@ -30,11 +60,46 @@ app.get("/signup", function (req, res) {
 });
 
 app.get("/view", function (req, res) {
-  res.render("view.html");
+  const sessionCookie = req.cookies.session || "";
+
+  admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+    .then(() => {
+      res.render("view.html");
+    })
+    .catch((error) => {
+      res.redirect("/login");
+    });
 });
 
 app.get("/", function (req, res) {
   res.render("index.html");
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+    .auth()
+    .createSessionCookie(idToken, { expiresIn })
+    .then(
+      (sessionCookie) => {
+        const options = { maxAge: expiresIn, httpOnly: true };
+        res.cookie("session", sessionCookie, options);
+        res.end(JSON.stringify({ status: "success" }));
+      },
+      (error) => {
+        res.status(401).send("UNAUTHORIZED REQUEST!");
+      }
+    );
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
 });
 
 app.listen(PORT, () => { console.log(`Server listening on port ${PORT}`); });
