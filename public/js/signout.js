7c8cbeed5dd23a0a67835eec629c077181dbae21
Christopher-John Cassar <christopherjcassar@gmail.com>
2021-01-19 14:47:08 +0000

    - Added signout function

---
+++ public/js/signout.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", () =>
+{
+  const firebaseConfig = {
+    apiKey: "AIzaSyBhdymNUMHIMZqKKCLIfzeyQ3vV7Fl8cf8",
+    authDomain: "obivision-7645d.firebaseapp.com",
+    databaseURL: "https://obivision-7645d-default-rtdb.firebaseio.com",
+    projectId: "obivision-7645d",
+    storageBucket: "obivision-7645d.appspot.com",
+    messagingSenderId: "809563515817",
+    appId: "1:809563515817:web:a06de894ebd9115cbd72b8",
+    measurementId: "G-BL2T39VTVF"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+
+  document
+    .getElementById("logout")
+    .addEventListener("click", (event) =>
+    {
+      event.preventDefault();
+      firebase.auth().signOut()
+      .then(() =>
+      {
+        window.location.assign("/login");
+      });
+    });
+});
